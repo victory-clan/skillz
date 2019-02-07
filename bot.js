0f4895597db1c9c67dadd8467827e779359d1a64
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-02-07 02:59:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,32 +7,8 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const client = new Discord.Client();
- 
-client.on('ready', function(){
- console.log(`Logged in as ${client.user.tag}!`);
-    
-   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
-   var s = ['483055660209012736','480169573530861578','483055655800930315'];
-    setInterval(function (){
-    client.user.setPresence({
- game: { 
-    type: 1,
-     url: 'https://www.twitch.tv/skwadraa',
-    name: '_help / by !Bako gaming#6414',
-    application_id: '541681966404075520',
-     assets: {
-         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
-  
-    }
-  }
-    });
-    }, 5000);
-});
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
- 
-  console.log('')
+ console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
   console.log(`[Start] ${new Date()}`);
@@ -53,6 +29,27 @@ const client = new Discord.Client();
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+});
+
+client.on('ready', function(){
+ console.log(`Logged in as ${client.user.tag}!`);
+    
+   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
+   var s = ['483055660209012736','480169573530861578','483055655800930315'];
+    setInterval(function (){
+    client.user.setPresence({
+ game: { 
+    type: 1,
+     url: 'https://www.twitch.tv/skwadraa',
+    name: '_help / by !Bako gaming#6414',
+    application_id: '541681966404075520',
+     assets: {
+         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
+  
+    }
+  }
+    });
+    }, 5000);
 });
 
 var prefix = "_" 
